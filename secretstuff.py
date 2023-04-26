e5dca49f37a86b4c2d72b74711998c7887a0e4c3
GENPBNJ <45128565+GENPBNJ@users.noreply.github.com>
2023-04-26 12:25:42 -0500

    db change

--- secretstuff.py
+++ secretstuff.py
@@ -1,4 +1,11 @@
-dbhost = 'localhost'
-dbuser = 'root'
-dbpass = 'f1t53K161B*l'
+#local
+# dbhost = 'localhost'
+# dbuser = 'root'
+# dbpass = 'f1t53K161B*l'
+# dbname = 'board_games'
+
+dbhost = 'us-cdbr-east-06.cleardb.net'
+dbuser = 'b6eea5019017de'
+dbpass = '15368e29'
 dbname = 'board_games'
+
